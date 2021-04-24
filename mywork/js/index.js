263cd955c2d5a8c1d07668277ed167df49d52dfb
seogaeun <seo323333@naver.com>
2021-04-24 14:39:06 +0900

    web-basics___project__mywork
    mywork__incheonAirport clone coding

--- mywork/js/index.js
+++ mywork/js/index.js
@@ -1,7 +1,4 @@
-// 햄버거 바 아이콘 선택자
-const menuOpenEl = document.getElementById('btn--open')
-// 닫는 아이콘 선택자
-const menuCloseEl = document.getElementById('btn--close')
+
 // 사이드 네비게이션 선택자
 const sideMenuEl = document.getElementById('side__menu')
 
@@ -17,72 +14,34 @@ if(window.scrollY>=200){
     //아니면(스크롤 위치가 200미만이면)
     //헤더요소의 높이를 원래대로
     // 아이콘을 보이지 않게 처리하고,사이드 메뉴 숨기기
-    headerMenuEl.style.height = '500px'
+    headerMenuEl.style.height = '80px'
     sideMenuEl.style.transform = 'translateY(-150%)'
     menuOpenEl.style.display = 'none'
     menuCloseEl.style.display = 'none'
-        // 햄버거 바 아이콘을 보이지않게 처리하고,
-        menuOpenEl.style.display = 'none'
-        // 닫는 아이콘은 보이지않게 처리합니다
-        menuCloseEl.style.display = 'none'
 }
 })
 
 //헤더에 올려지는 이벤트가 발생하면
 headerMenuEl.addEventListener("mouseover", function() {
-    // 햄버거 바 아이콘을 보이게 처리하고,
-            menuOpenEl.style.display = 'none'
-    // 닫는 아이콘은 보이지않게 처리합니다
-            menuCloseEl.style.display = 'block'
     // 사이드 메뉴를 보이게 합니다 
             sideMenuEl.style.transform = 'translateY(0%)'
 })
 
   //헤더에 벗어나는 이벤트가 발생하면
      headerMenuEl.addEventListener("mouseout", function() {
-    // 햄버거 바 아이콘을 보이지않게 처리하고,
-             menuOpenEl.style.display = 'none'
-    // 닫는 아이콘은 보이지게 처리합니다
-            menuCloseEl.style.display = 'block'
     // 사이드 메뉴를 보이지 않게 합니다 
             sideMenuEl.style.transform = 'translateY(-150%)'
 })
-    // 햄버거 바 아이콘에 올려지는 이벤트가 발생하면,
-menuOpenEl.addEventListener("mouseover", function() {
-    // 햄버거 바 아이콘을 보이지 않게 처리하고,
-    menuOpenEl.style.display = 'none'
-    // 닫는 아이콘은 보이게 처리합니다
-    menuCloseEl.style.display = 'block'
-    // 사이드 메뉴를 보이게 합니다 
-    sideMenuEl.style.transform = 'translateY(0%)'
-})
 
-// 닫는 아이콘에 벗어나는 이벤트가 발생하면,
-menuCloseEl.addEventListener("mouseout", function() {
-    // 햄버거 바 아이콘을 보이게 처리하고,
-    menuOpenEl.style.display = 'block'
-    // 닫는 아이콘은 보이지 않게 처리합니다
-    menuCloseEl.style.display = 'none'
-    // 사이드 메뉴를 원상태로 숨깁니다
-    sideMenuEl.style.transform = 'translateY(-150%)'
-})
 
 //네비게이션에 올려지는 이벤트가 발생하면
 sideMenuEl.addEventListener("mouseover",function(){
-     // 햄버거 바 아이콘을 보이지 않게 처리하고,
-     menuOpenEl.style.display = 'none'
-     // 닫는 아이콘은 보이게 처리합니다
-     menuCloseEl.style.display = 'block'
      // 사이드 메뉴를 보이게 합니다 
      sideMenuEl.style.transform = 'translateY(0%)'
 })
 
 //네비게이션에서 벗어나는 이벤트가 발생하면
 sideMenuEl.addEventListener("mouseout",function(){
-    // 햄버거 바 아이콘을 보이게 처리하고,
-     menuOpenEl.style.display = 'block'
-     // 닫는 아이콘은 보이지 않게 처리합니다
-     menuCloseEl.style.display = 'none'
     // 사이드 메뉴를 원상태로 숨깁니다
      sideMenuEl.style.transform = 'translateY(-150%)'
 })
